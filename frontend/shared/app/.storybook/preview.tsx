6cd875f787527a75218f7cd4a3f82d51fa6b96a0
Chinnawat <chinnawat.w@opsta.co.th>
2025-03-26 17:54:10 +0700

    refactor styles in Storybook and Button component; remove Preline imports from CSS and preview files

--- frontend/shared/app/.storybook/preview.tsx
+++ frontend/shared/app/.storybook/preview.tsx
@@ -1,6 +1,5 @@
 import type { Preview } from '@storybook/react';
 import React, { useEffect } from 'react';
-import 'preline'; // ✅ just import, DO NOT call it
 import '../src/index.css'; // CSS ที่มี Tailwind + Preline styles
 
 const withPreline = (Story: any) => {
--- frontend/shared/app/src/stories/components/button.tsx
+++ frontend/shared/app/src/stories/components/button.tsx
@@ -1,6 +1,6 @@
 export default function Button() {
     return (
-        <div className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-hidden focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
+        <div className="border-2 border-red-500">
             hello
         </div>
     );
